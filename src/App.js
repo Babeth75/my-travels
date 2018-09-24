4c590f90538deb7c1d04a74af489eace9bf66525
Babeth <elisabeth.jousselin@hotmail.fr>
2018-09-24 17:45:37 +0200

    Afficher une liste de composants

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Travel from "./Travel";
+import Travels from "./Travels";
 
 class App extends Component {
   render() {
@@ -13,18 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">My Travel</h1>
         </header>
-        <Travel
-          destination="Vulcano"
-          country="Italie"
-          photo="http://www.eolclub.com/FRANCIA/vulcani/Le%20volcanisme%20de%20l'%C3%AEle%20de%20Vulcano_file/vulcano.jpg"
-          distance="2181 km"
-        />
-        <Travel
-          destination="New-York"
-          country="États-Unis"
-          photo="https://cdn.shopify.com/s/files/1/1248/6797/files/original_grande.jpg?v=1480110873"
-          distance="5 834 km"
-        />
+        <Travels/>
       </div>
     );
   }
---
+++ src/Travels.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Travel from './Travel';
+
+const travels = [
+  {
+    id:1,
+    destination:"New-York",
+    country:"Etat-Unis",
+    photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/NYC_-_Time_Square_-_From_upperstairs.jpg/1200px-NYC_-_Time_Square_-_From_upperstairs.jpg",
+    distance:"5245 km"
+  },
+  {
+    id:2,
+    destination:"Paris",
+    country:"France",
+    photo:"https://www.riotgames.com/darkroom/1440/b2b587d91d3c5d2922953ac62fbb2cb8:dfd0d5c2d07f981fb8cda29623b5e54e/paris.jpg",
+    distance:"0 km"
+  },
+  {
+    id:3,
+    destination:"Los Angeles",
+    country:"Etats-unis",
+    photo:"https://www.breizh-info.com/wp-content/uploads/2018/01/Hollywood_Sign_Zuschnitt-e1515409906782.jpg",
+    distance:"8654 km"
+  },
+  {
+    id:4,
+    destination:"Londres",
+    country:"Angleterre",
+    photo:"https://thumbs.dreamstime.com/b/big-ben-londres-32915756.jpg",
+    distance:"2181 km"
+  }
+];
+
+const Travels = () => (
+    <div>
+        {
+          travels.map((travel,index) => (
+            <Travel key={index} {...travel} />
+          ))
+        }
+    </div>
+  );
+  
+export default Travels;
